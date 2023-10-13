9761011e9a2ffc97102c522b4ffbf56eb315d799
pankaj <pankaj.awasthi@novoinvent.com>
2023-10-13 16:53:01 +0530

    adding class object

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,31 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 235, 181, 22),
-              Color.fromARGB(255, 246, 222, 5),
-              Color.fromARGB(255, 11, 237, 30)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const Center(
-            child: Text("Hello"),
-          ),
-        ),
-      ),
+    const MaterialApp(
+      home: Scaffold(backgroundColor: Colors.amber, body: GradientContainer()),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 235, 181, 22),
+          Color.fromARGB(255, 246, 222, 5),
+          Color.fromARGB(255, 11, 237, 30)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello",
+          style: TextStyle(color: Colors.white, fontSize: 28),
+        ),
+      ),
+    );
+  }
 }
